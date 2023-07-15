9f9b34c95c87f4c32d9c79e0632fb6796b9d1775
hadi <hadi.mr90@hotmail.com>
2023-07-15 20:57:55 +0330

    ..

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -74,28 +74,37 @@ WSGI_APPLICATION = 'chatter_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+import pymysql
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'railway',
-#         'USER': 'root',
-#         'PASSWORD': 'bRU1u7xp6wIkLnaPEKlB',
-#         'HOST': 'containers-us-west-44.railway.app',
-#         'PORT': '7412',
-#     }
-# }
+pymysql.version_info = (2, 2, 0, "final", 0) # to pass Django's compatibility check
+pymysql.install_as_MySQLdb() # to pretend as MySQLdb
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASS'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'bRU1u7xp6wIkLnaPEKlB',
+        'HOST': 'containers-us-west-44.railway.app',
+        'PORT': '7412',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('DATABASE_NAME'),
+#         'USER': os.environ.get('DATABASE_USER'),
+#         'PASSWORD': os.environ.get('DATABASE_PASS'),
+#         'HOST': os.environ.get('DATABASE_HOST'),
+#         'PORT': os.environ.get('DATABASE_PORT'),
+#     }
+# }
+
+
 
 
 # Password validation
