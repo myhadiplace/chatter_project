e5d462dd3d66629413a45d5219cfd6affcc301a5
hadi <hadi.mr90@hotmail.com>
2023-07-15 18:42:42 +0330

    ...

--- chatter_project/settings.py
+++ chatter_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +75,25 @@ WSGI_APPLICATION = 'chatter_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'railway',
+#         'USER': 'root',
+#         'PASSWORD': 'bRU1u7xp6wIkLnaPEKlB',
+#         'HOST': 'containers-us-west-44.railway.app',
+#         'PORT': '7412',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'bRU1u7xp6wIkLnaPEKlB',
-        'HOST': 'containers-us-west-44.railway.app',
-        'PORT': '7412',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASS'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
